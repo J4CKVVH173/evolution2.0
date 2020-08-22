defc3183f0c0b3fe9b30e0eefe8976d2c4500b51
Andrey <i78901234567890@gmail.com>
2020-08-22 11:13:01 +0300

    fix. Опечатка

--- cells/live/herbivore.py
+++ cells/live/herbivore.py
@@ -10,7 +10,7 @@ class Herbivore(BaseLive):
 
     def make_move(self) -> None:
         gen = self.get_gen()
-        # ToDo оформить вввиде цепочки мб, подумать
+        # ToDo оформить ввиде цепочки мб, подумать
         if gen in [0, 1, 2, 3]:
             self._move_info.set_move_y(-1)  # вверх
         if gen in [4, 5, 6, 7]:
