6b89d8bba95f080111931464b9260c62a0590a29
Andrey <i78901234567890@gmail.com>
2020-08-30 12:13:40 +0300

    fix. Оптимизация, if исправлен на elif

--- cells/live/herbivore.py
+++ cells/live/herbivore.py
@@ -10,14 +10,13 @@ class Herbivore(BaseLive):
 
     def make_move(self) -> None:
         gen = self.get_gen()
-        # ToDo оформить ввиде цепочки мб, подумать
         if gen in [0, 1, 2, 3]:
             self._move_info.set_move_y(-1)  # вверх
-        if gen in [4, 5, 6, 7]:
+        elif gen in [4, 5, 6, 7]:
             self._move_info.set_move_x(1)  # вправо
-        if gen in [8, 9, 10, 11]:
+        elif gen in [8, 9, 10, 11]:
             self._move_info.set_move_y(1)  # вниз
-        if gen in [12, 13, 14, 15]:
+        elif gen in [12, 13, 14, 15]:
             self._move_info.set_move_x(-1)  # влево
 
         self._reducing_health()
